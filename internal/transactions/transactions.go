a2e40ba33f87263f9e21d955265cf1534fb1ae6b
Martin Ndeto <martindeto@gmail.com>
2025-03-13 00:58:47 +0300

    Parse RPC Response, Correct polling go routines

--- internal/transactions/transactions.go
+++ internal/transactions/transactions.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	"txpool-viz/config"
+
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 type RPCRequest struct {
@@ -19,28 +21,45 @@ type RPCRequest struct {
 	Jsonrpc string
 }
 
+type RPCResponse struct {
+	JSONRPC string `json:"jsonrpc"`
+	ID      int    `json:"id"`
+	Result  Result `json:"result"`
+}
+
+type Result struct {
+	Pending map[string]map[string]*types.Transaction `json:"pending"`
+	Queued  map[string]map[string]*types.Transaction `json:"queued"`
+}
+
 func PollTransactions(cfg *config.Config) {
 	for _, endpoint := range cfg.Endpoints {
 		go func(endpoint config.Endpoint) {
-			ticker := time.NewTicker((cfg.Polling["interval"]))
+			ticker := time.NewTicker(cfg.Polling["interval"])
 			defer ticker.Stop()
 
 			for range ticker.C {
 				ctx, cancel := context.WithTimeout(context.Background(), cfg.Polling["timeout"])
+				defer cancel()
+
+				done := make(chan struct{})
+
 				go func() {
-					defer cancel()
 					getTransactions(ctx, endpoint)
+					close(done)
 				}()
+
 				select {
+				case <-done:
 				case <-ctx.Done():
 					fmt.Printf("Transaction polling for %s timed out\n", endpoint.Name)
-				case <-time.After(cfg.Polling["timeout"]):
 				}
 			}
 		}(endpoint)
 	}
 	select {}
 }
+
 
 func getTransactions(ctx context.Context, endpoint config.Endpoint) {
 	fmt.Println("Getting transactions from", endpoint.Name)
@@ -88,5 +107,28 @@ func getTransactions(ctx context.Context, endpoint config.Endpoint) {
 		return
 	}
 
-	fmt.Println(string(responseData))
+	var rpcResponse RPCResponse
+	if err = json.Unmarshal(responseData, &rpcResponse); err != nil {
+		fmt.Println("Error unmarshalling response data:", err)
+		return
+	}
+
+	pending, err := json.Marshal(rpcResponse.Result.Pending)
+
+	if err != nil {
+		fmt.Println("Error marshalling pending transactions:", err)
+		return
+	}
+
+	queued, err := json.Marshal(rpcResponse.Result.Queued)
+
+	if err != nil {
+		fmt.Println("Error marshalling queued transactions:", err)
+		return
+	}
+
+	fmt.Println("Pending transactions:")
+	fmt.Println(string(pending))
+	fmt.Println("Queued transactions:")
+	fmt.Println(string(queued))
 }
