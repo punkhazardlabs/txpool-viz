21e47156ab0cce22704ee2360111b6cc6de6440c
Martin Ndeto <martindeto@gmail.com>
2025-04-03 01:59:56 +0300

    Live stream, track transactions across all clients

--- config/config.go
+++ config/config.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/ethereum/go-ethereum/ethclient"
 	_ "github.com/joho/godotenv/autoload"
 	"gopkg.in/yaml.v3"
 )
@@ -14,6 +15,7 @@ type Endpoint struct {
 	RPCUrl      string						`yaml:"rpc_url"`
 	Websocket   string						`yaml:"socket"`
 	AuthHeaders map[string]string `yaml:"auth_headers"`
+	Client			*ethclient.Client
 }
 
 type Config struct {
@@ -36,6 +38,17 @@ func Load() (*Config, error) {
 		throwErr := fmt.Errorf("Error parsing config file: %v", err)
 		panic(throwErr)
 	}
+
+	// Create clients for the Endpoints
+	for i := range userConfig.Endpoints {
+		client, err := ethclient.Dial(userConfig.Endpoints[i].RPCUrl)
+
+		if err != nil {
+			return nil, fmt.Errorf("Error connecting to client %s", userConfig.Endpoints[i].Name)
+		}
+
+		userConfig.Endpoints[i].Client = client
+	}	
 
 	return userConfig, nil
 }
